69d1de1f948ec1191f4ed20c7a813a38c95fd722
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-08-04 16:58:53 -0700

    Update pt_resnet.py

--- cross_framework_hpo/resnet/pt_resnet.py
+++ cross_framework_hpo/resnet/pt_resnet.py
@@ -4,7 +4,7 @@ import pytorch_lightning as pl
 import torch
 
 
-def mnist_pt_objective(config):
+def resnet_pt_objective(config):
     torch.manual_seed(0)
     model_class = BasePytorchModel(config)
     model_class.model = models.resnet50(pretrained=False)
@@ -22,4 +22,4 @@ def mnist_pt_objective(config):
 if __name__ == "__main__":
     test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'dropout': 0.5, 'adam_epsilon': 1e-7}
     # test_config = {'batch_size': 800, 'learning_rate': 0.0001955, 'epochs': 15, 'dropout': 0.8869, 'adam_epsilon': 0.1182}
-    res = mnist_pt_objective(test_config)
+    res = resnet_pt_objective(test_config)
