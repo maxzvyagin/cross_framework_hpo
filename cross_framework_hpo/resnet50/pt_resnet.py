2d414e7bbd94b918d3dfb63cf5ade9748eda3e84
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-08-04 17:18:37 -0700

    adding vgg for tensorflow

--- cross_framework_hpo/resnet50/pt_resnet.py
+++ cross_framework_hpo/resnet50/pt_resnet.py
@@ -8,7 +8,6 @@ def resnet_pt_objective(config):
 
 
 if __name__ == "__main__":
-    test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'dropout': 0.5, 'adam_epsilon': 1e-7}
-    # test_config = {'batch_size': 800, 'learning_rate': 0.0001955, 'epochs': 15, 'dropout': 0.8869, 'adam_epsilon': 0.1182}
+    test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'adam_epsilon': 1e-7}
     res = resnet_pt_objective(test_config)
     print("Accuracy is {}".format(res[0]))
