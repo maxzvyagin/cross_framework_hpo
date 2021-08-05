8d3ddd8624a6c44f72f919bb4c46bc67cfbfcbc5
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-08-04 17:16:23 -0700

    Update pt_vgg.py

--- cross_framework_hpo/vgg16/pt_vgg.py
+++ cross_framework_hpo/vgg16/pt_vgg.py
@@ -1,26 +1,14 @@
-from cross_framework_hpo.base_pytorch_model import BasePytorchModel
+from cross_framework_hpo.base_pytorch_model import base_pytorch_function
 import torchvision.models as models
-import pytorch_lightning as pl
-import torch
 
 
-def resnet_pt_objective(config):
-    torch.manual_seed(0)
-    model_class = BasePytorchModel(config)
-    model_class.model = models.vgg16(pretrained=False)
-    model_class.model.train()
-    try:
-        trainer = pl.Trainer(max_epochs=config['epochs'], gpus=[0])
-    except:
-        print("WARNING: training on CPU only, GPU[0] not found.")
-        trainer = pl.Trainer(max_epochs=config['epochs'])
-    trainer.fit(model_class)
-    trainer.test(model_class)
-    return (model_class.test_accuracy, model_class.model, model_class.avg_training_loss_history, model_class.latest_training_loss_history)
+def vgg_pt_objective(config):
+    model = models.vgg16(pretrained=False)
+    return base_pytorch_function(config, supplied_model=model)
 
 
 if __name__ == "__main__":
     test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'dropout': 0.5, 'adam_epsilon': 1e-7}
     # test_config = {'batch_size': 800, 'learning_rate': 0.0001955, 'epochs': 15, 'dropout': 0.8869, 'adam_epsilon': 0.1182}
-    res = resnet_pt_objective(test_config)
+    res = vgg_pt_objective(test_config)
     print("Accuracy is {}".format(res[0]))
