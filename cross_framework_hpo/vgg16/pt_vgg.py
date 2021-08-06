b1df39f8da2a506d8d1e6c662dea49cb8375b1f8
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-08-05 18:34:39 -0700

    updating vgg16, so torch has same number of params

--- cross_framework_hpo/vgg16/pt_vgg.py
+++ cross_framework_hpo/vgg16/pt_vgg.py
@@ -1,9 +1,10 @@
 from cross_framework_hpo.base_pytorch_model import base_pytorch_function
-import torchvision.models as models
+# import torchvision.models as models
+from cross_framework_hpo.vgg16.updated_torchvision_vgg import vgg16
 
 
 def vgg_pt_objective(config):
-    model = models.vgg16(pretrained=False, num_classes=100)
+    model = vgg16(pretrained=False, num_classes=100)
     return base_pytorch_function(config, supplied_model=model)
 
 
@@ -11,3 +12,5 @@ if __name__ == "__main__":
     test_config = {'batch_size': 64, 'learning_rate': .001, 'epochs': 1, 'adam_epsilon': 1e-7}
     res = vgg_pt_objective(test_config)
     print("Accuracy is {}".format(res[0]))
+
+
