10bb82fcb4eaa2fdb26b334c23c36d35e05973cf
maxzvyagin <59709404+maxzvyagin@users.noreply.github.com>
2021-08-22 15:25:31 -0700

    why why why was tensorflow the wrong number of classes aah

--- cross_framework_hpo/resnet50/tf_resnet.py
+++ cross_framework_hpo/resnet50/tf_resnet.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from cross_framework_hpo.base_tensorflow_model import base_tensorflow_function
 
 def resnet_tf_objective(config):
-    model = tf.keras.applications.resnet50.ResNet50(weights=None, input_shape=(3, 32, 32), classes=100)
+    model = tf.keras.applications.resnet50.ResNet50(weights=None, input_shape=(3, 32, 32), classes=10)
     return base_tensorflow_function(config=config, model=model)
 
 if __name__ == "__main__":
